f7b7659c01b2b68bd1c581871423214981c5089c
Philipp Rathmanner <yarmek@icloud.com>
2016-08-29 09:02:53 +0200

    testing

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -76,7 +76,7 @@ author = 'Michael Reithinger, Philipp Rathmanner, Lukas Tanner, Philipp Grandits
 # built documents.
 #
 # The short X.Y version.
-version = '0.3.3'
+version = '0.3.4'
 # The full version, including alpha/beta/rc tags.
 #release = '0.3.3'
 
